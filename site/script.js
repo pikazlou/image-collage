ad03da91c228710e1bf9ecd6741cbfd22cade4e6
Pavel Kazlou <p.i.kazlou@gmail.com>
2023-08-19 14:01:23 +0200

    modal for cropping image according to tile size

--- site/script.js
+++ site/script.js
@@ -1,3 +1,6 @@
+var tiles = [[0, 0, 5, 5], [5, 0, 3, 3], [8, 0, 4, 3], [5, 3, 4, 3], [9, 3, 3, 3], [12, 0, 3, 3], [0, 5, 5, 4], [5, 6, 3, 3], [12, 3, 3, 4], [0, 9, 4, 4], [4, 9, 4, 5], [8, 6, 4, 4], [12, 7, 3, 3], [8, 10, 3, 4], [11, 10, 4, 3], [11, 13, 4, 4], [0, 13, 4, 3], [4, 14, 3, 3], [0, 16, 4, 4], [7, 14, 4, 3], [4, 17, 4, 3], [8, 17, 3, 3], [11, 17, 4, 3]];
+var selected_tile;
+
 $(document).ready(function(){
 
     var multiplier = 20;
@@ -6,26 +9,27 @@ $(document).ready(function(){
     const ctx = can.getContext('2d');
     const can_rect = can.getBoundingClientRect();
 
-    let tiles = [[0, 0, 5, 5], [5, 0, 3, 3], [8, 0, 4, 3], [5, 3, 4, 3], [9, 3, 3, 3], [12, 0, 3, 3], [0, 5, 5, 4], [5, 6, 3, 3], [12, 3, 3, 4], [0, 9, 4, 4], [4, 9, 4, 5], [8, 6, 4, 4], [12, 7, 3, 3], [8, 10, 3, 4], [11, 10, 4, 3], [11, 13, 4, 4], [0, 13, 4, 3], [4, 14, 3, 3], [0, 16, 4, 4], [7, 14, 4, 3], [4, 17, 4, 3], [8, 17, 3, 3], [11, 17, 4, 3]]
     draw_tiles(tiles, -1, multiplier, ctx);
 
     $('#canvas').click(function(e) {
         ctx.clearRect(0, 0, can.width, can.height);
         let mouse_x = e.clientX;
         let mouse_y = e.clientY;
-        let selected_tile = find_selected_tile(mouse_x - can_rect.left, mouse_y - can_rect.top, tiles, multiplier);
+        selected_tile = find_selected_tile(mouse_x - can_rect.left, mouse_y - can_rect.top, tiles, multiplier);
         draw_tiles(tiles, selected_tile, multiplier, ctx);
     });
 });
 
 function draw_tiles(tiles, selected_tile, multiplier, canvas_ctx) {
     canvas_ctx.strokeStyle = '#ff0000';
+    canvas_ctx.lineWidth = 1;
     for (i in tiles) {
         draw_single_tile(tiles[i], multiplier, canvas_ctx)
     }
 
-    canvas_ctx.strokeStyle = '#00ff00';
     if (selected_tile >= 0) {
+        canvas_ctx.strokeStyle = '#00ff00';
+        canvas_ctx.lineWidth = 5;
         draw_single_tile(tiles[selected_tile], multiplier, canvas_ctx)
     }
 }
@@ -60,3 +64,116 @@ function find_selected_tile(event_x, event_y, tiles, multiplier) {
     }
     return -1;
 }
+
+
+window.addEventListener('DOMContentLoaded', function () {
+  //var avatar = document.getElementById('avatar');
+  var image = document.getElementById('image');
+  var input = document.getElementById('input');
+  var $progress = $('.progress');
+  var $progressBar = $('.progress-bar');
+  var $alert = $('.alert');
+  var $modal = $('#modal');
+  var cropper;
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  input.addEventListener('change', function (e) {
+    var files = e.target.files;
+    var done = function (url) {
+      input.value = '';
+      image.src = url;
+      $alert.hide();
+      $modal.modal('show');
+    };
+    var reader;
+    var file;
+    var url;
+
+    if (files && files.length > 0) {
+      file = files[0];
+
+      if (URL) {
+        done(URL.createObjectURL(file));
+      } else if (FileReader) {
+        reader = new FileReader();
+        reader.onload = function (e) {
+          done(reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  });
+
+  $modal.on('shown.bs.modal', function () {
+    let tile_width = tiles[selected_tile][2];
+    let tile_height = tiles[selected_tile][3];
+    let aspectRatio = 1.0 * tile_width / tile_height;
+    cropper = new Cropper(image, {
+      aspectRatio: aspectRatio,
+      viewMode: 3,
+    });
+  }).on('hidden.bs.modal', function () {
+    cropper.destroy();
+    cropper = null;
+  });
+
+  document.getElementById('crop').addEventListener('click', function () {
+    //var initialAvatarURL;
+    var canvas;
+
+    $modal.modal('hide');
+
+    if (cropper) {
+      canvas = cropper.getCroppedCanvas({
+        //width: 160,
+        //height: 160,
+      });
+      //initialAvatarURL = avatar.src;
+      //avatar.src = canvas.toDataURL();
+      $progress.show();
+      $alert.removeClass('alert-success alert-warning');
+      canvas.toBlob(function (blob) {
+        var formData = new FormData();
+
+        formData.append('avatar', blob, 'avatar.jpg');
+        $.ajax('https://jsonplaceholder.typicode.com/posts', {
+          method: 'POST',
+          data: formData,
+          processData: false,
+          contentType: false,
+
+          xhr: function () {
+            var xhr = new XMLHttpRequest();
+
+            xhr.upload.onprogress = function (e) {
+              var percent = '0';
+              var percentage = '0%';
+
+              if (e.lengthComputable) {
+                percent = Math.round((e.loaded / e.total) * 100);
+                percentage = percent + '%';
+                $progressBar.width(percentage).attr('aria-valuenow', percent).text(percentage);
+              }
+            };
+
+            return xhr;
+          },
+
+          success: function () {
+            $alert.show().addClass('alert-success').text('Upload success');
+          },
+
+          error: function () {
+            //avatar.src = initialAvatarURL;
+            $alert.show().addClass('alert-warning').text('Upload error');
+          },
+
+          complete: function () {
+            $progress.hide();
+          },
+        });
+      });
+    }
+  });
+});
